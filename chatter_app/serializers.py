e71f67c38eb5c2792aeb6bc2bb3195d5b4483625
mdarsie <github@darsie.me>
2020-01-31 15:03:22 -0500

    add comment serializer

--- chatter_app/serializers.py
+++ chatter_app/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from .models import User, Patter, Comment, Like
 
+
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
-        fields = ('id')
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
 
