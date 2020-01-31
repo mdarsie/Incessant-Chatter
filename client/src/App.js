b8f10e4c3adf99603bfd426f497519f137bf11ca
mdarsie <github@darsie.me>
2020-01-31 16:51:09 -0500

    render all tweets on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 import "./App.css";
 
 class App extends React.Component {
-  
-  componentDidMount(){
-    axios.get('/api/v1/patter')
-    .then((res) => {
-      console.log(res.data)
-    })
+  state = {
+    patter: []
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get("/api/v1/patter");
+      this.setState({ patter: res.data });
+    } catch (err) {
+      console.log("failed to retrieve patter");
+    }
   }
-  
+
   render() {
     return (
       <div>
         <h1>Hello World</h1>
+        {this.state.patter.map(patter => {
+          return <div>{patter.content}</div>;
+        })}
       </div>
     );
   }
