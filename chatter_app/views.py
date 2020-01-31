61e912aa69cf3c502a8e8ac7e5ba45644dd84caa
mdarsie <github@darsie.me>
2020-01-31 15:22:57 -0500

    add user, patter, comment and like views

--- chatter_app/views.py
+++ chatter_app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import UserSerializer, PatterSerializer, CommentSerializer, LikeSerializer
+from .models import User, Patter, Comment, Like
+
+
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class PatterView(viewsets.ModelViewSet):
+    queryset = Patter.objects.all()
+    serializer_class = PatterSerializer
+
+
+class CommentView(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class LikeView(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
