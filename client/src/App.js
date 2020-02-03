dfc6dace6f21d626b656df865474b53927710d6e
mdarsie <github@darsie.me>
2020-02-03 11:48:43 -0500

    add new patter option to homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,75 @@
 import React from "react";
 import axios from "axios";
 import "./App.css";
+import Patter from "./components/Patter";
 
 class App extends React.Component {
   state = {
-    patter: []
+    patters: [],
+    newPatter: {
+      content: "",
+      image_url: "",
+      user: 1
+    }
   };
 
   async componentDidMount() {
     try {
-      const response = await axios.get("/api/v1/patter");
-      this.setState({ patter: response.data });
+      const res = await axios.get("/api/v1/patter");
+      this.setState({ patters: res.data });
     } catch (err) {
-      console.log("failed to retrieve patter");
+      console.log("Failed to retrieve data");
     }
   }
+
+  onNewPatterChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+    const newState = { ...this.state };
+
+    newState.newPatter[name] = value;
+
+    this.setState(newState);
+  };
+
+  onNewPatter = () => {
+    axios.post("/api/v1/patter", this.state.newPatter);
+  };
 
   render() {
     return (
       <div>
-        <h1>Incessant Chatter</h1>
+        <h1>INCESSANT CHATTER</h1>
         <div>
-            <textarea class='new-patter-input' placeholder='Who cares?'/>
-            <button>Patter</button>
-        {this.state.patter.map(patter => {
-          return <div>{patter.content}</div>;
+          <textarea
+            className="create-patter-text"
+            placeholder="What's Happening"
+            name="content"
+            onChange={this.onNewPatterChange}
+            value={this.state.newPatter.content}
+          />
+
+          <input
+            type="text"
+            placeholder="Image URL"
+            name="image_url"
+            onChange={this.onNewPatterChange}
+            value={this.state.newPatter.image_url}
+          />
+
+          <button onClick={this.onNewPatter}>Patter</button>
+
+        </div>
+        {this.state.patters.map(patter => {
+          return (
+            <Patter
+              picture_url={patter.user.picture_url}
+              name={patter.user.name}
+              handle={patter.user.handle}
+              likes={patter.likes.length}
+              content={patter.content}
+            />
+          );
         })}
       </div>
     );
